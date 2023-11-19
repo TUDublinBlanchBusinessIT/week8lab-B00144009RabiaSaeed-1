1e998668ac1feaf440f98a78c9a01f1f9da4fc53
B00144009RabiaSaeed <B00144009@mytudublin.ie>
2023-11-19 17:36:13 +0000

    These two from submission

---
+++ CarPolicy2.php
@@ -0,0 +1,61 @@
+<?php
+//I submitted this in class but I forgot to commit it//
+//Here is the car policy and it's private attributes//
+class CarPolicy
+{
+    private $policyNumber = 0;
+    private $yearlyPremium = 0;
+    private $dateOfLastClaim = "";
+    //private policyHolder = "";//
+    
+    //this is the constuctor function, there's two going into this//
+    public function __construct($policyNumber, $yearlyPremium)
+    {
+        $this->policyNumber = $policyNumber;
+        $this->yearlyPremium = $yearlyPremium;
+    }
+    //policies that don't have a claim between the years three and five get a 10% discount, policies that don't have a claim for more then five years get 15% discount//
+    //I only know how to make functions so I'm just gopnna make two functions instead of adding it into one//
+    public function setDateOfLastClaim($dateOfLastClaim)
+    {
+        $this->dateOfLastClaim = $dateOfLastClaim;
+    }
+    
+    public function getTotalYearsNoClaims()
+    {
+        $currentDate = new DateTime();
+        $lastDate= new DateTime($this->dateOfLastClaim);
+        $interval = $currentDate->diff($lastDate);
+        return $interval->format("%y");
+    }
+    
+    public function getDiscount()
+    {
+        //
+        //we need to get it right here//
+        $yearOfNoClaim = $this->getTotalYearsNoClaims();
+        if ($yearOfNoClaim >= 3 && $yearOfNoClaim>= 5){
+            return 0.10;
+        }
+            
+        elseif ($yearOfNoClaim > 5){
+            return 0.15;
+        }
+    }
+    
+        
+    public function getDiscountedPremium()
+    {
+        $theDiscount = $this->getDiscount();
+        $discountedPremium = $this->yearlyPremium * (1 - $theDiscount);
+        return $discountedPremium;
+    }
+    
+    //there's a mistake in the question, there can't be a " : " for this, if you run the test what I did will work//
+    public function __toString()
+    {
+        return $this->policyNumber;
+        
+    }
+        
+}
---
+++ processCarPolicy.php
@@ -0,0 +1,7 @@
+<?php
+//I forgot to add this too so I'll just put it here//
+//baby step done for this, file made and file included//
+include('CarPolicy2.php');
+
+    
+
