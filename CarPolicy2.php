218db25d87207d2820b5c98fbf23cf02b4ab59c4
B00144009RabiaSaeed <B00144009@mytudublin.ie>
2023-11-19 17:42:44 +0000

    Added comments

--- CarPolicy2.php
+++ CarPolicy2.php
@@ -43,7 +43,8 @@ class CarPolicy
         }
     }
     
-        
+   //this is for the discounted premium//
+//It's for yearly premium so the right amount should be subtracted//
     public function getDiscountedPremium()
     {
         $theDiscount = $this->getDiscount();
